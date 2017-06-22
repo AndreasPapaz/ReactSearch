ee525a980bed4d09c48cb4e6f41ffc829166bf14
Andreas <Andreas@Andreas.local>
2017-06-22 18:31:48 -0500

    update, debugging webpack errors

---
+++ app/components/children/results.js
@@ -0,0 +1,34 @@
+var React = require("react");
+
+var Results = React.createClass({
+	onClickSave: function(i) {
+		this.props.setSave(i);
+	},
+	render: function() {
+		return (
+			<div className="row">
+				<div className="col-sm-12">
+					<div className="panel panel-success">
+					<div className="panel-heading">Results</div>
+					<div id="results" className="panel-body">
+						{this.props.aricles.map((search, i) => {
+							return (
+								<div className="panel panel-default" key={i}>
+									 <div className="panel-heading">{search.web_url}<button className="pull-right btn btn-info" onClick={() => this.onClickSave(i)} id={i}>Save</button>
+										<div className="clearfix"></div>
+									</div>
+									<div className="panel-body">
+										<p>{search.abstract} <br />Date: {search.pub_date}</p>
+										<hr />
+									</div>
+								</div>
+							);
+						})}
+					</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+});
+module.exports = Results;
--- routes/routes.js
+++ routes/routes.js
@@ -12,3 +12,40 @@ router.get("/api/saved", function(req,res) {
 		}
 	});
 });
+
+router.post("/api/saved", function(req, res) {
+	var newArticle = new Article({
+		title: req.body.abstract,
+		date: req.body.pub_date,
+		url: req.body.web_url
+	});
+
+	newArticle.save(function(err, article) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.json('pass');
+		}
+	});
+});
+
+router.delete("/api/saved/:id", function(req, res) {
+	Article.findByIdAndRemove(req.params.id, function(err, article) {
+		if (err){
+			console.log(err)
+		} else {
+			res.json("pass");
+		}
+	});
+});
+
+//LOOK HERE< THIs is where my problems lie
+router.get("*", function(req, res) {
+	var dir = __dirname;
+	var dirSplit = dir.split("controller");
+	dir = dirSplit[0];
+
+	res.sendFile(dir + '/public/index.html');
+});
+
+module.exports = router;
